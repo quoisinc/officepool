0b7ea34a562f08c6dcaa8a045dcf532db88df1ce
Elliott James <jamese@Elliotts-iMac.home>
2014-12-29 19:06:50 -0500

    added style assets

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp', ['ui.router']);
 
 myApp.config(function($stateProvider,$urlRouterProvider){
 	$stateProvider.state('home',{
-		url : '/home',
+		url : '/',
 		templateUrl: 'partials/index.html'
 	}).
 	state('about',{
